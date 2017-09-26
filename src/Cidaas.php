cd34ecda91775dce2cc47bed5d3762eeeb6ff758
Cidaas <ganesh.kumar@widas.in>
2017-09-26 09:01:25 +0530

    Update Cidaas.php

--- src/Cidaas.php
+++ src/Cidaas.php
@@ -2,6 +2,7 @@
 namespace Cidaas\OAuth2\Client\Provider;
 
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
@@ -28,9 +29,14 @@ class Cidaas extends AbstractProvider
         return $this->domain() . '/oauth2-login/oauth2/authz';
     }
 
-    public function getManagerUserInfo()
+    public function getManagerUserInfoUrl()
     {
         return $this->domain() . '/oauth2-usermanagement/oauth2/user';
+    }
+
+    public function getUserInfoUrl()
+    {
+        return $this->domain() . '/token/userinfobytoken';
     }
 
 
@@ -143,7 +149,7 @@ class Cidaas extends AbstractProvider
     public function getUserInfoById(AccessToken $token,$user_id){
         $client = new Client();
 
-        $result = $client->get($this->getManagerUserInfo()."/".$user_id,[
+        $result = $client->get($this->getManagerUserInfoUrl()."/".$user_id,[
             "headers"=>[
                 "Content-Type" => "application/json",
                 "access_token"=>$token->getToken()
@@ -153,4 +159,139 @@ class Cidaas extends AbstractProvider
         return $this->createResourceOwner($this->parseResponse($result), $token);
 
     }
+
+    public function getUserInfo(Request $request){
+
+        $access_token_key = "access_token";
+
+        $access_token = null;
+
+        if( $request->headers->has($access_token_key)) {
+            $access_token = $request->headers->get($access_token_key);
+        }
+
+        if($access_token==null && $request->query($access_token_key) != null){
+            $access_token = $request->query($access_token_key);
+        }
+
+        if($access_token==null && $request->headers->has("authorization")){
+            $auth = $request->headers->get("authorization");
+
+            if(strtolower(substr($auth, 0, strlen("bearer"))) === "bearer"){
+                $authvals = explode(" ",$auth);
+
+                if(sizeof($authvals)>1){
+                    $access_token = $authvals[1];
+                }
+            }
+        }
+
+        $cookieRequest = false;
+        if($access_token==null && $request->cookies->get("access_token")){
+            $access_token = $request->cookies->get("access_token");
+            $cookieRequest = true;
+        }
+
+
+        if($access_token == null)
+        {
+            return response()->json(["error"=>"Access denied for this resource"], 401);;
+        }
+
+
+
+        $ipAddress = "";
+
+        if($request->headers->has("x-forwarded-for")){
+            $ips = explode(" ",$request->headers->get("x-forwarded-for"));
+            $ipAddress = explode(",",$auth)[0];
+        }
+
+
+        $host = "";
+
+        if($request->headers->has("X-Forwarded-Host")){
+            $host = $request->headers->get("X-Forwarded-Host");
+        }
+
+        $acceptLanguage = "";
+
+        if($request->headers->has("Accept-Language")){
+            $acceptLanguage = $request->headers->get("Accept-Language");
+        }
+
+        $userAgent = "";
+
+        if($request->headers->has("user-agent")){
+            $userAgent = $request->headers->get("user-agent");
+        }
+
+        $referrer = "";
+
+        if($request->headers->has("referrer")){
+            $referrer = $request->headers->get("referrer");
+        }
+
+        $allHeaders = [];
+        foreach ($request->headers as $key=>$value){
+            $allHeaders[$key] = $value[0];
+        }
+
+
+
+        $dataToSend = [
+            "accessToken"=>$access_token,
+            "userId"=>null,
+            "clientId"=>null,
+            "referrer"=>$referrer,
+            "ipAddress"=>$ipAddress,
+            "host"=>$host,
+            "acceptLanguage"=>$acceptLanguage,
+            "userAgent"=>$userAgent,
+            "requestURL"=>"",
+            "success"=>false,
+            "requestedScopes"=>"",
+            "requestedRoles"=>"",
+            "createdTime"=>date_create('now')->format('Y-m-d\TH:i:sO'),
+            "requestInfo"=>$allHeaders
+        ];
+
+
+
+
+        $roles = $request->route()->getAction("roles");
+
+        if($roles!=null){
+            $dataToSend["requestedRoles"] =  implode(",",$roles);
+        }
+
+        $scopes = $request->route()->getAction("scopes");
+
+        if($scopes!=null){
+            $dataToSend["requestedScopes"] =  implode(" ",$scopes);
+        }
+
+
+        $client = new Client();
+
+        $result = $client->post($this->getUserInfoUrl(),[
+            "json"=>$dataToSend,
+            "headers"=>[
+                "Content-Type" => "application/json",
+                "access_token"=>$access_token
+            ]
+        ]);
+
+        if($result->getStatusCode() == 200) {
+            $token_check_response = json_decode($result->getBody()->getContents());
+
+
+            return $token_check_response;
+
+        }
+
+        return null;
+
+    }
+
 }
