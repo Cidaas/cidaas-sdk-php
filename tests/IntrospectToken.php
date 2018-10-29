42baeba8a09e5de3ac3c5d71fac9c8556e2cfce4
vimal prakash <vimal.prakash@widas.in>
2018-10-30 01:16:53 +0530

    fixes

--- tests/IntrospectToken.php
+++ tests/IntrospectToken.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Cidaas\OAuth2\Client\Provider\Cidaas;
 
 $provider = new Cidaas([
-    'base_url' => 'https://nightlybuild.cidaas.de',
+    'base_url' => 'https://cidaas-base-url.cidaas.de',
     'client_id' => '3e4ad34e-97c5-410d-82c9-1d9a71820a87', // The client ID assigned to you by the provider
     'client_secret' => 'cf914b42-6a0e-48a1-aea6-935bfa749027', // The client password assigned to you by the provider
 ]);
