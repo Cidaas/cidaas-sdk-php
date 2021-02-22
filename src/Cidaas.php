e74ba4c128d11e4ffa8b9158cfc35ad57a390a37
Michael Schempp <hemi@lj-hemi.de>
2021-02-22 13:29:06 +0100

    added phpdoc fpr new parameter

--- src/Cidaas.php
+++ src/Cidaas.php
@@ -121,6 +121,7 @@ class Cidaas {
      * Register new user.
      * @param array $registrationFields containing user data according to {@see getRegistrationSetup()}
      * @param string $requestId for user registration
+     * @param string $acceptLanguage for the language. defaults to "en-GB"
      * @return PromiseInterface promise with user data or error
      */
     public function register(array $registrationFields, string $requestId, string $acceptLanguage = 'en-GB'): PromiseInterface {
